945987c68c5e27c2e2f88d807ba7e6fb29222e20
Ibro <ibro@ibro.me>
2017-03-26 16:55:38 +0200

    tuples and dictionary

--- TupleType.ts
+++ TupleType.ts
@@ -20,8 +20,30 @@ export default function TupleTypeInit() {
     interface ITuple {
         0: boolean,
         1: number
-    };
+    }
 
     let y: ITuple;
     y = [false, 0, 3, 3, true];
+
+
+    type IDictionaryTupleItem = [string, number];
+    type IDictionaryTuple = IDictionaryTupleItem[];
+
+    let myDictionary: IDictionaryTuple;
+    myDictionary['10'] = 5;
+
+    myDictionary['key1'] = 5;
+    myDictionary['key2'] = 'some text';
+
+    myDictionary['yes'] = true;
+    myDictionary['yes'] = 5;
+
+    interface IDictionary {
+        [index: string]: number
+    }
+
+    let dict: IDictionary;
+    dict['key1'] = 5;
+    dict['key2'] = 1;
+    dict[5] = 1;
 }
